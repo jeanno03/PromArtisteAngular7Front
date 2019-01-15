b438c890fdbcd01b1b0c141ac55060707ccb35d6
jeanno03 <jeannory.phou@gmail.com>
2019-01-15 08:57:39 +0100

    conection ok v2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
 export const environment = {
   production: true,
       // copy firebase configuration from your firebase project
-      firebaseConfig:  {    
-      apiKey: "AIzaSyBvwo4P2HxKbMNX-f-_2ZiHuD9bJDtHAno",
-      authDomain: "promartiste.firebaseapp.com",
-      databaseURL: "https://promartiste.firebaseio.com",
-      projectId: "promartiste",
-      storageBucket: "promartiste.appspot.com",
-      messagingSenderId: "1089657118584"
-}
+
+
 };
